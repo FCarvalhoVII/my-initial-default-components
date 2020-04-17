737040f493bbfd0f0b1d7da0e19e8c9250156c32
Felipe Carvalho <felipecarvalho1915@hotmail.com>
2020-04-17 16:44:51 -0300

    Adds Storybook config

---
+++ .storybook/theme.js
@@ -0,0 +1,20 @@
+import { create } from '@storybook/theming'
+
+export default create({
+    base: 'dark',
+
+    colorSecondary: '#141414',
+
+    appBg: '#0a0c0f',
+    appContentBg: '#1a1a1f',
+    appBorderColor: '#141414',
+
+    textColor: '#fff',
+
+    // Toolbar default and active colors
+    barTextColor: '#d7d7d7',
+    barSelectedColor: '#fff',
+    barBg: '#191b1f',
+
+    brandTitle: 'Kit of Components'
+})
