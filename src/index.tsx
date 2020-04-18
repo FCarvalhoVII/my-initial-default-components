4271af2808d2861820e6d1d2d5a62f1142d541a6
Felipe Carvalho <felipecarvalho1915@hotmail.com>
2020-04-18 14:55:06 -0300

    Adds File of exports

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+export { Button } from './components/Button';
+export { ButtonGradient } from './components/ButtonGradient';
+export { Separator } from './components/Separator';
+export { Radio } from './components/Radio';
+export { CheckBox } from './components/CheckBox';
+export { Input } from './components/Input';
+export { Modal } from './components/Modal';
+export { Card } from './components/Card';
