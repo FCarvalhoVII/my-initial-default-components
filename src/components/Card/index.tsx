8493d3e26c9387e90448046953e9fcff717f6d3a
Felipe Carvalho <felipecarvalho1915@hotmail.com>
2020-06-08 16:43:35 -0300

    Adds customization to the card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -2,17 +2,29 @@ import React from 'react'
 
 import { Container } from './styles'
 
-interface Props {
+export interface Props {
+    backgroundColor?: string
+    borderRadius?: string
+    color?: string
     title: string
 }
 
 export const Card: React.FC<Props> = ({
+    backgroundColor = '#2f2e35',
+    borderRadius = '10px',
+    color = '#ddd',
     title,
     children,
     ...props
 }) => {
     return (
-        <Container title={title} {...props}>
+        <Container
+            backgroundColor={backgroundColor}
+            borderRadius={borderRadius}
+            color={color}
+            title={title} 
+            {...props}
+        >
             <h2>{ title }</h2>
 
             { children }
--- src/components/Card/styles.ts
+++ src/components/Card/styles.ts
@@ -1,19 +1,21 @@
 import styled from 'styled-components'
 
-export const Container = styled.div`
+import { Props } from './index'
+
+export const Container = styled.div<Props>`
     display: flex;
     flex-direction: column;
     align-items: center;
     padding: 20px;
     margin: 20px;
-    border-radius: 10px;
+    border-radius: ${props => props.borderRadius || '10px'};
     box-shadow: 1px 5px 5px 1px rgba(0, 0, 0, 0.77);
 
-    background-color: #2f2e35;
-    color: #ddd;
+    background-color: ${props => props.backgroundColor || '#2f2e35'};
+    color: ${props => props.color || '#ddd'};
 
     h2 {
-        color: #ddd;
+        color: ${props => props.color || '#ddd'};
         margin-bottom: 10px; 
     }
 `;
