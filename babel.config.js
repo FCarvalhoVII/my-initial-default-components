6178c531f2a57d1a7a60ad841d347b3908f9ba04
Felipe Carvalho <felipecarvalho1915@hotmail.com>
2020-06-10 17:02:14 -0300

    Delete babel-jest plugin for causing error

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,8 @@ module.exports = function (api) {
     api.cache(true);
   
     const presets = [];
-    const plugins = [{"^.+\\.[t|j]sx?$": "babel-jest"},];
   
     return {
         presets,
-        plugins
     };
 }
